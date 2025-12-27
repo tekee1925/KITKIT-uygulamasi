b6766fc0a1878bf8d981c6f952fedc64facc71c6
Talha Yasin Teke <tekee1925@gmail.com>
2025-12-27 19:47:57 +0300

    Firebase config eklendi - site çalışsın

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Configuration
+export const firebaseConfig = {
+    apiKey: "AIzaSyDkaUp7nJU_QPDqRQk6b8Q5XorEY5W8VOE",
+    authDomain: "kitkit-5f8e2.firebaseapp.com",
+    projectId: "kitkit-5f8e2",
+    storageBucket: "kitkit-5f8e2.firebasestorage.app",
+    messagingSenderId: "603659165842",
+    appId: "1:603659165842:web:9cf1626a8e6365ee23464b",
+    measurementId: "G-44PSXTTZ4D"
+};
